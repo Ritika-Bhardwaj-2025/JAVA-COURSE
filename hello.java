0ab9428179fcb8de4954452403017394d0d99e7d
ritika8803 <ritika.bhardwaj2021@vitbhopal.ac.in>
2025-02-15 11:00:24 +0530

    learn today about the compiling,var, literals.

---
+++ hello.java
@@ -0,0 +1,21 @@
+class hello{
+    public static void main(String a[]){
+        // int num1 = 2;
+        // int num2 = 3;
+        // int result = num1 + num2;
+        // System.out.println(result);
+
+        int r = 5;
+        byte b = 127;
+        short c = 24;
+        long d = 6468l;
+
+        float e = 5.6f;
+        double f = 5.67;
+
+        char g = 'K';
+
+        boolean h = true;
+    }
+}
+
